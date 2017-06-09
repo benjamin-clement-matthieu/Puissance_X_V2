9e959a8478a93be3e22f7feb2c3029dcbe5cb340
benjamin-clement-matthieu <clement.ansel14@gmail.com>
2017-06-09 15:33:24 +0200

    Ajout des options Difficultés et Puissance Mode

--- src/Options/Valeurs.java
+++ src/Options/Valeurs.java
@@ -1,11 +1,13 @@
 package Options;
 
+import IA.IADifficulte;
 import Moteur.PuissanceMode;
 
 public class Valeurs {
 	// Par défaut puissance 4
 	private static PuissanceMode puissanceMode = PuissanceMode.PUISSANCE4;
-		
+	private static IADifficulte iaDifficulte = IADifficulte.MOYEN;
+	
 	public static void setPuissanceMode(PuissanceMode mode)
 	{
 		puissanceMode = mode;
@@ -14,5 +16,15 @@ public class Valeurs {
 	public static PuissanceMode getPuissanceMode()
 	{
 		return puissanceMode;
+	}
+	
+	public static void setIADifficulte(IADifficulte difficulte)
+	{
+		iaDifficulte = difficulte;
+	}
+		
+	public static IADifficulte getIADifficulte()
+	{
+		return iaDifficulte;
 	}
 }
